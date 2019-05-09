d38e1599875160d594fbb64ba05ce7b9abc6f9eb
tranduy1441997 <49270829+tranduy1441997@users.noreply.github.com>
2019-05-09 17:09:47 +0700

    5

--- app/src/main/java/com/example/myapplication/Now_playing.kt
+++ app/src/main/java/com/example/myapplication/Now_playing.kt
@@ -10,5 +10,25 @@ class Now_playing: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.now_playing, container, false)
     }
+    /*override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.themoviedb.org/3/movie/now_playing?api_key=7519cb3f829ecd53bd9b7007076dbe23")
+            .build()
+        client.newCall(request)
+            .enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if(response.isSuccessful){
+
+                    }
+                }
+            })
+    }*/
 
 }
